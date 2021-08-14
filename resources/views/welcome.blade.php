edd8fa43f29966d6f17d011d3134ddf3eb1fb638
Dalpat Singh <dalpatsingh@innowaysit.com>
2021-08-14 22:18:23 +0530

    recently added 4 news in welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 
 @section('content')
     <div class="container">
+        {{-- crops block --}}
         <div class="row my-3 bg-light p-3 rounded">
             <div class="col-12">
                 <div class="clearfix">
@@ -31,7 +32,38 @@
                 </div>
             </div>
             @endforeach
-
         </div>
+        {{-- end of crops block --}}
+
+        {{-- news block --}}
+        <div class="row my-3 bg-light p-3 rounded">
+            <div class="col-12">
+                <div class="clearfix">
+                    <div class="float-left">
+                        Recent news
+                    </div>
+                    <div class="float-right">
+                        <a href="{{ route('news.index') }}" class="btn btn-outline-primary">View All</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            @foreach ($recent_news as $news)
+            <div class="col-12 col-md-3">
+                <div class="card h-100">
+                    <img class="card-img-top w-100 img-fluid img-thumbnail p-4 border-0 rounded" src="{{ asset('storage/'.$news->thumbnail) }}" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">{{ $news->title }}</h4>
+                    </div>
+
+                    <div class="card-footer">
+                        <a class="btn btn-primary" href="{{ route('news.show', $news->id) }}">View</a>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        {{-- end of news block --}}
     </div>
 @endsection

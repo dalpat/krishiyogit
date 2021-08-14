0cf4ad65befecc86c28479a66469c8e1774cc02b
Dalpat Singh <dalpatsingh@innowaysit.com>
2021-08-14 22:24:29 +0530

    showing crops farmer name

--- resources/views/crops/show.blade.php
+++ resources/views/crops/show.blade.php
@@ -16,6 +16,7 @@
                         <h4 class="card-title">{{ $crop->title }}</h4>
                         <p class="card-text"><i class="fa fa-inr" aria-hidden="true"></i>
                             {{ $crop->price . '/' . $crop->unit }}</p>
+                            <p class="card-text">Farmer: {{ $crop->farmer->name }}</p>
                         <button type="button" class="btn btn-primary">Buy Now</button> <br>
                         <a href="#details" class="btn btn-link">View details</a>
                     </div>
@@ -28,6 +29,7 @@
                 <p>
                     {{ $crop->description }}
                 </p>
+                <p>Provided by: {{ $crop->farmer->name }}</p>
             </div>
         </div>
     </div>
